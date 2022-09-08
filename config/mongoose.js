83e4043b4678fcbcf11270fb1e871aa7dba7a899
ankushk1 <ankush.kapoor@hudini.io>
2022-09-08 17:02:58 +0530

    fix

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const dotenv = require("dotenv");
 dotenv.config()
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/oct21",{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/oct21",{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
