21e69e3a6e8f4677e61c7d67380f7be3a517e282
Ankush <ankushkapoor179@gmail.com>
2022-09-08 20:57:13 +0530

    Added heroku files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+require('dotenv').config()
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const bodyParser = require("body-parser");
 const db = require("./config/mongoose");
 const userRoutes = require("./routes/userRoutes");
